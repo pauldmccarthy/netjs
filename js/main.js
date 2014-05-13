1ac23c3ead4e90fbbb929b3cf6e934b55a47aba5
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-05-13 15:15:30 +0100

    Tweak to threshold function, as I think it was still buggy

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ require(["netjs", "lib/d3"], function(netjs, d3) {
 
   function thresholdMatrix(matrix) {
 
+    var thresMatrix = [];
+
     for (var i = 0; i < matrix.length; i++) {
 
       // TODO We are assuming here that the matrix
@@ -11,15 +13,17 @@ require(["netjs", "lib/d3"], function(netjs, d3) {
       absVals   = matrix[i].map(function(val) {return Math.abs(val);});
       nodeThres = d3.max(absVals) * 0.75;
 
-      for (var j = i+1; j < matrix[i].length; j ++) {
-        if (Math.abs(matrix[i][j]) < nodeThres) {
-          matrix[i][j] = Number.NaN;
-          matrix[j][i] = Number.NaN;
-        }
+      thresMatrix.push([]);
+
+      for (var j = 0; j < matrix[i].length; j++) {
+        if (Math.abs(matrix[i][j]) < nodeThres) 
+          thresMatrix[i].push(Number.NaN);
+        else 
+          thresMatrix[i].push(matrix[i][j]);
       }
     }
 
-    return matrix;
+    return thresMatrix;
   }
 
 
@@ -30,6 +34,12 @@ require(["netjs", "lib/d3"], function(netjs, d3) {
   urls.nodeLabels   =  "/data/dataset2/clusters.txt";
   urls.linkage      =  "/data/dataset2/linkages.txt";
   urls.thumbnails   =  "/data/dataset2/melodic_IC_sum.sum";
+
+  // urls.matrices     = ["/data/dummy/corr1.txt", "/data/dummy/corr2.txt"];
+  // urls.matrixLabels = ["Corr1", "Corr2"];
+  // urls.nodeLabels   =  "/data/dummy/clusters.txt";
+  // urls.linkage      =  "/data/dummy/linkages.txt";
+  // urls.thumbnails   =  "/data/dummy/thumbnails";
 
   netjs.loadNetwork(urls, thresholdMatrix, function(net) {
 
