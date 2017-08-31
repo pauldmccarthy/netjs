360438616ac4ff4f5fb6d7d66ea08c0569aa0fbc
Paul McCarthy <pauldmccarthy@gmail.com>
2017-08-31 11:13:11 -0700

    Refactoring with the intent that a python script will be used to generate a js report. Things are broken

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ require(["netjs", "lib/d3"], function(netjs, d3) {
 
     for (var i = 0; i < matrix.length; i++) {
       absVals = matrix[i].map(function(val) {return Math.abs(val);});
-      nodeThress.push(d3.max(absVals) * thresPerc); 
+      nodeThress.push(d3.max(absVals) * thresPerc);
     }
 
     for (var i = 0; i < matrix.length; i++) {
@@ -24,7 +24,7 @@ require(["netjs", "lib/d3"], function(netjs, d3) {
             Math.abs(matrix[i][j]) < nodeThress[j])
 
           thresMatrix[i].push(Number.NaN);
-        else 
+        else
           thresMatrix[i].push(matrix[i][j]);
       }
     }
@@ -40,7 +40,7 @@ require(["netjs", "lib/d3"], function(netjs, d3) {
   //      initial values. See the loadNetwork
   //      function in netdata.js for detail on all
   //      arguments.
-  
+
   //
   //    - The second one ('display' here) is passed
   //      to the displayNetwork function, and specifies
@@ -51,32 +51,77 @@ require(["netjs", "lib/d3"], function(netjs, d3) {
   var args             = {};
   var display          = {};
 
-  args.matrices        = ["data/dataset2/Znet1.txt", "data/dataset2/Znet2.txt"];
-  args.matrixLabels    = ["Corr1", "Corr2"];
-  args.nodeData        = ["data/dataset2/clusters.txt"];
-  args.nodeDataLabels  = ["Cluster number"];
-  args.nodeNames       = ["data/dataset2/names.txt"];
-  args.nodeNameLabels  = ["Names"];
-  args.linkage         =  "data/dataset2/linkages.txt";
-  args.nodeOrders      = ["data/dataset2/order1.txt"];
-  args.nodeOrderLabels = ["Order 1"];
-  args.thumbnails      =  "data/dataset2/melodic_IC_sum.sum";
+  var matrices      = document.getElementById("matrices")     .getAttribute("data");
+  var linkages      = document.getElementById("linkages")     .getAttribute("data");
+  var names         = document.getElementById("names")        .getAttribute("data");
+  var orders        = document.getElementById("orders")       .getAttribute("data");
+  var groups        = document.getElementById("groups")       .getAttribute("data");
+  var thumbnails    = document.getElementById("thumbnails")   .getAttribute("data");
+  var matrix_labels = document.getElementById("matrix_labels").getAttribute("data");
+  var name_labels   = document.getElementById("name_labels")  .getAttribute("data");
+  var order_labels  = document.getElementById("order_labels") .getAttribute("data");
+  var group_labels  = document.getElementById("group_labels") .getAttribute("data");
+
+  args.matrices = matrices.split("|");
+
+  if (linkages      !== "") args.linkages        = linkages     .split("|");
+  if (names         !== "") args.nodeNames       = names        .split("|");
+  if (orders        !== "") args.nodeOrders      = orders       .split("|");
+  if (groups        !== "") args.nodeData        = groups       .split("|");
+  if (thumbnails    !== "") args.thumbnails      = thumbnails   .split("|");
+  if (matrix_labels !== "") args.matrixLabels    = matrix_labels.split("|");
+  if (name_labels   !== "") args.nodeNameLabels  = name_labels  .split("|");
+  if (order_labels  !== "") args.nodeOrderLabels = order_labels .split("|");
+  if (group_labels  !== "") args.nodeGroupLabels = group_labels .split("|");
+
+  console.log(args.matrices);
+  console.log(args.linkages);
+  console.log(args.nodeNames);
+  console.log(args.nodeOrders);
+  console.log(args.nodeData);
+  console.log(args.thumbnails);
+  console.log(args.matrixLabels);
+  console.log(args.nodeNameLabels);
+  console.log(args.nodeOrderLabels);
+  console.log(args.nodeGroupLabels);
+
+  // you can specify a custom
+  // thresholding function here
   args.thresFunc       = thresholdMatrix;
   args.thresVals       = [0.75];
-  args.thresLabels     = ["Thres perc"];
+  args.thresLabels     = ["Thres percentage"];
   args.thresholdIdx    = 0;
-  args.nodeNameIdx     = -1;
-  args.nodeOrderIdx    = -1;
+
+  // Set to -1 to use node indices as their names
+  args.nodeNameIdx     = 0;
+
+  // Set to -1 to lay out nodes
+  // according to the dendrogram
+  args.nodeOrderIdx    = 0;
+
+  // intial number of clusters
   args.numClusters     = 10;
+
+  // Draw nodes this many pixels
+  // from the canvas edge
+  display.nodeRadiusOffset = 150;
+
+  // Draw edges this many
+  // pixels from the nodes
+  display.edgeRadiusOffset = 20;
+
+  // Node radius
+  display.nodeSize = 5;
+
+  display.networkDiv    = "#fullNetwork";
+  // display.subNetDiv     = "#subNetwork";
+  display.controlDiv    = "#networkCtrl";
+
 
   // Figure out a sensible canvas size.
   var w  = window.innerWidth  - 200;
   var h  = window.innerHeight - 50;
   var sz = Math.min(w/2.0, h);
-
-  display.networkDiv    = "#fullNetwork";
-  display.subNetDiv     = "#subNetwork";
-  display.controlDiv    = "#networkCtrl";
   display.networkWidth  = sz;
   display.networkHeight = sz;
   display.subNetWidth   = sz;
@@ -84,10 +129,9 @@ require(["netjs", "lib/d3"], function(netjs, d3) {
 
   display.highlightOn   = true;
 
-  // Load the network, and 
+  // Load the network, and
   // display it when loaded.
   netjs.loadNetwork(args, function(net) {
     netjs.displayNetwork(net, display);
   });
 });
-
--- scripts/bids2netjs.py
+++ scripts/bids2netjs.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 #
-# bids2netjs.py -
+# bids2netjs.py - Generate a netjs report from a compatible BIDS
+#                 connectivity derivatives data set.
 #
 # Author: Paul McCarthy <pauldmccarthy@gmail.com>
 #
 """
+
 Need to generate/extract
  - connectivity matrix
  - node labels
@@ -16,12 +18,12 @@ Need to generate/extract
 
 
 Output directory has following contents:
- - matrixN.txt       - connectivity matrix N
- - hierarchyN.txt    - Hierarchy for connectivity matrix N
- - labelsXX.txt      - Node labels
- - ordersXX.txt      - Node ordering
- - groupsXX.txt      - Node grouping
- - thumbnailMMMM.png - Node MMMM thumbnail
+ - matrixN.txt                  - connectivity matrix N
+ - hierarchyN.txt               - Hierarchy for connectivity matrix N
+ - labelsXX.txt                 - Node labels
+ - ordersXX.txt                 - Node ordering
+ - groupsXX.txt                 - Node grouping
+ - thumbnails/thumbnailMMMM.png - Node MMMM thumbnail
 """
 
 
@@ -31,6 +33,7 @@ import os.path       as op
 import                  math
 import                  shutil
 
+import jinja2        as j2
 import bids.grabbids as grabbids
 
 
@@ -58,19 +61,23 @@ def getNodeGroups(layout, indir, outdir):
 
 def getConnectivityFile(layout, indir, outdir, src, dest):
 
-    netmats = layout.get(conndata='network', extensions=src)
+    netmats   = layout.get(conndata='network', extensions=src)
 
     if len(netmats) == 0:
-        raise RuntimeError('{} does not look like a FSLNets-'
-                           'BIDS data set!'.format(indir))
+        return []
 
     numdigits = math.floor(math.log10(len(netmats))) + 1
     fmt       = '{}{{:0{}d}}.txt'.format(dest, numdigits)
+    files     = []
 
     for i, nm in enumerate(netmats):
 
         dest = op.join(outdir, fmt.format(i))
         shutil.copy(nm.filename, dest)
+
+        files.append(dest)
+
+    return files
 
 
 def getNodeMeta(layout, outdir, key, prefix):
@@ -84,8 +91,8 @@ def getNodeMeta(layout, outdir, key, prefix):
 
     for i, (name, data) in enumerate(metasets.items()):
 
-        metamap[name] = i
         dest          = op.join(outdir, '{}{}.txt'.format(prefix, i))
+        metamap[name] = dest
 
         with open(dest, 'wt') as f:
             f.write('\n'.join([str(d) for d in data]))
@@ -96,13 +103,34 @@ def getNodeMeta(layout, outdir, key, prefix):
 def getThumbnails(layout, indir, outdir):
 
     thumbnails = layout.get(conndata='node', extensions='thumbnail.png')
+    tndir      = op.join(outdir, 'thumbnails')
+
+    os.makedirs(tndir)
 
     for thumb in thumbnails:
 
         index = int(thumb.index) - 1
-        dest  = op.join(outdir, 'thumbnail{:04d}.png'.format(index))
+        dest  = op.join(tndir, 'thumbnail{:04d}.png'.format(index))
 
         shutil.copy(thumb.filename, dest)
+
+    return tndir
+
+
+def getNetJs(namespace, outdir):
+
+    basedir      = op.dirname(__file__)
+    jsdir        = op.join(basedir, '..', 'js')
+    htmlTemplate = op.join(basedir, '..', 'index.html')
+
+    htmlTemplate = open(htmlTemplate, 'rt').read()
+    htmlTemplate = j2.Template(htmlTemplate)
+    html         = htmlTemplate.render(**namespace)
+
+    shutil.copytree(jsdir, op.join(outdir, 'js'))
+
+    with open(op.join(outdir, 'index.html'), 'wt') as f:
+        f.write(html)
 
 
 def main(args=None):
@@ -118,15 +146,58 @@ def main(args=None):
     basedir = op.dirname(__file__)
     config  = op.join(basedir, 'bids-connectivity-derivatives.json')
     layout  = grabbids.BIDSLayout(indir, config=config)
+    datadir = op.join(outdir, 'data')
 
-    if not op.exists(outdir):
-        os.makedirs(outdir)
+    if not op.exists(datadir):
+        os.makedirs(datadir)
 
-    getConnectivity(layout, indir, outdir)
-    getNodeLabels(  layout, indir, outdir)
-    getNodeOrders(  layout, indir, outdir)
-    getNodeGroups(  layout, indir, outdir)
-    getThumbnails(  layout, indir, outdir)
+    matrices   = getConnectivity(layout, indir, datadir)
+    linkages   = getHierarchy(   layout, indir, datadir)
+    names      = getNodeLabels(  layout, indir, datadir)
+    orders     = getNodeOrders(  layout, indir, datadir)
+    groups     = getNodeGroups(  layout, indir, datadir)
+    thumbnails = getThumbnails(  layout, indir, datadir)
+
+    matrices   = [op.relpath(f, outdir) for f in matrices]
+    linkages   = [op.relpath(f, outdir) for f in linkages]
+    names      = [op.relpath(f, outdir) for f in names .values()]
+    orders     = [op.relpath(f, outdir) for f in orders.values()]
+    groups     = [op.relpath(f, outdir) for f in groups.values()]
+    thumbnails =  op.relpath(thumbnails, outdir)
+
+    matrix_labels = [str(i) for i in range(len(matrices))]
+    name_labels   = [str(i) for i in range(len(names))]
+    group_labels  = [str(i) for i in range(len(groups))]
+    order_labels  = [str(i) for i in range(len(orders))]
+
+
+    if len(linkages) > 0: linkages = linkages[0]
+    else:                 linkages = ""
+
+    matrices = '|'.join(matrices)
+    names    = '|'.join(names)
+    orders   = '|'.join(orders)
+    groups   = '|'.join(groups)
+
+    matrix_labels = '|'.join(matrix_labels)
+    name_labels   = '|'.join(name_labels)
+    group_labels  = '|'.join(group_labels)
+    order_labels  = '|'.join(order_labels)
+
+    namespace               = {}
+    namespace['matrices']   = matrices
+    namespace['linkages']   = linkages
+    namespace['names']      = names
+    namespace['orders']     = orders
+    namespace['groups']     = groups
+    namespace['thumbnails'] = thumbnails
+
+    namespace['matrix_labels'] = matrix_labels
+    namespace['name_labels']   = name_labels
+    namespace['group_labels']  = group_labels
+    namespace['order_labels']  = order_labels
+
+    getNetJs(namespace, outdir)
 
 
 if __name__ == '__main__':
